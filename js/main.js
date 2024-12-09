7eed621c3594d24756c5f6496488c3f2b2e19fee
enzo <enzovcnt@gmail.com>
2024-12-09 15:49:24 +0100

    communication with IA

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
-alert("Hello World!");
+//identifiant : pierre : XqSn5rM35sQT
+
+let token = null
+
+
+
+let params = {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        username: "pierre",
+        password: "XqSn5rM35sQT",
+    })
+}
+
+fetch('https://felix.esdlyon.dev/login', params)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.token)
+        token = data.token
+        console.log("coucou",token)
+        answer()
+
+
+    });
+
+
+
+function answer(){
+    let paramsIA = {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + token
+    },
+    body: JSON.stringify({
+        prompt : "salut je m'appelle enzo"
+    })
+}
+    fetch('https://felix.esdlyon.dev/ollama', paramsIA)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    });
+}
+
+if(!token){
+    displayLoginPage()
+}else{
+    displayChatInterface()
+}
