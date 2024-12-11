b604d842cf4fa279cb0056070515c06217f32a6a
enzo <enzovcnt@gmail.com>
2024-12-11 18:19:59 +0100

    add profile pictures and move to the right the message of the user and to the left the message of the IA

--- js/main.js
+++ js/main.js
@@ -2,29 +2,86 @@
 //identifiant : enzo : jwIAKDpQjq
 
 let token = null
+const loginPage = document.querySelector('.login')
+const chatPage = document.querySelector('.chat')
+
+let premierMessageIA = {
+    author : "Felix",
+    content : "Bonjour je suis l'IA",
+}
+
+let premierMessageUser = {
+    author : "enzo",
+    content : "Bonjour je suis enzo. Quel est la couleur du ciel ?",
+}
+let deuxiemeMessageIA = {
+    author : "Felix",
+    content : "Quelle est ta question ?",
+}
+
+//let params = { //avoir le token
+//    method: "POST",
+//    headers: {
+  //      'Content-Type': 'application/json'
+    //},
+    //body: JSON.stringify({
+      //  username: "enzo",
+        //password: "jwIAKDpQjq",
+    //})
+//}
+
+let messages = [premierMessageIA, premierMessageUser, deuxiemeMessageIA]
+async function login(username, password){
+    console.log(username, password)
+    let params = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        })
+    }
+
+    return await fetch('https://felix.esdlyon.dev/login', params)
+        .then((response) =>  response.json())
+        .then((json) => {
+
+            return json.token
+        })
+}
 
 
+function displayLoginForm(){
 
-let params = { //avoir le token
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        username: "enzo",
-        password: "jwIAKDpQjq",
+    loginPage.style.display = 'block'
+    chatPage.style.display = 'none'
+
+    let username = document.querySelector('.inputUsername')
+    let password = document.querySelector('.inputPassword')
+    let loginButton = document.querySelector('.loginBtn')
+    loginButton.addEventListener('click', ()=>{
+
+        login(username.value, password.value).then((data) => {
+            token = data
+            displayChat()
+            //console.log(token)
+        })
     })
 }
 
-fetch('https://felix.esdlyon.dev/login', params)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.token)
-        token = data.token
-        console.log("coucou",token)
-        answer()
 
-    });
+
+//fetch('https://felix.esdlyon.dev/login', params)
+  //  .then(response => response.json())
+    //.then(data => {
+      //  console.log(data.token)
+        //token = data.token
+        //console.log("coucou",token)
+        //answer()
+
+    //});
 
 
 
@@ -46,8 +103,102 @@ function answer(){
     });
 }
 
+
+function displayMessages(){
+    document.querySelector('.messages').innerHTML = ""
+    messages.forEach(message => {
+
+        const profilPic = document.createElement("img");
+        profilPic.classList.add('profil');
+        divMessage = document.createElement('div')
+        divMessage.classList.add('message')
+        let paragraphe = document.createElement('p')
+
+        paragraphe.textContent = message.content
+        divMessage.appendChild(profilPic)
+        divMessage.appendChild(paragraphe)
+
+
+        if(message.author === "Felix")
+        {
+            divMessage.classList.add('felix')
+
+            profilPic.src = "https://picsum.photos/id/48/100";
+
+        }else{
+            divMessage.classList.add('user')
+            paragraphe.classList.add('pUser')
+            profilPic.src = "https://picsum.photos/100";
+
+        }
+        document.querySelector('.messages').appendChild(divMessage)
+    })
+
+
+
+}
+
+function handlePrompt(){
+    let prompt = document.querySelector('.inputMessage')
+    let submitButton = document.querySelector('.sendBtn')
+
+    submitButton.addEventListener('click', ()=>{
+        addMessageToMessagesArray({
+            author : "User",
+            content:prompt.value
+        })
+        displayMessages()
+
+
+        askIa(prompt.value).then((data) => {
+            console.log(data)
+            addMessageToMessagesArray({
+                author : "Felix",
+                content:data
+            })
+            displayMessages()
+        })
+    })
+}
+
+async function askIa(prompt)
+{
+    let params = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            prompt: prompt,
+        })
+    }
+    return await fetch('https://felix.esdlyon.dev/ollama', params)
+        .then(response => response.json())
+        .then((json) => {
+            console.log(json)
+            return json.message
+        })
+}
+
+function addMessageToMessagesArray(message)
+{
+    messages.push(message)
+}
+
+function displayChat(){
+    chatPage.style.display = 'block'
+    loginPage.style.display = 'none'
+
+    displayMessages()
+    handlePrompt()
+}
+
+
+
 if(!token){
-    displayLoginPage()
+    displayLoginForm()
 }else{
-    displayChatInterface()
+    displayChat()
+    displayMessages()
 }
